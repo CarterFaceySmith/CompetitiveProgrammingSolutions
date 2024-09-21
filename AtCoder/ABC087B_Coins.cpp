a004e362d7e6181ce864cb5da851cf8f88830524
Carter <carterfs@proton.me>
2024-09-22 09:16:11 +1000

    feat: Complete ABC087B Coins

--- AtCoder/ABC087B_Coins.cpp
+++ AtCoder/ABC087B_Coins.cpp
@@ -5,7 +5,6 @@
 */
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
@@ -14,6 +13,17 @@ int main() {
 
   int A, B, C, X;
   cin >> A >> B >> C >> X;
+  int count = 0;
 
+  for (int a = 0; a <= A; a++){
+    for (int b = 0; b <= B; b++){
+      for (int c = 0; c <= C; c++){
+        if (((a * 500) + (b * 100) + (c * 50)) == X) count++;
+        else if (((a * 500) + (b * 100) + (c * 50)) > X) break;
+      }
+    }
+  }
 
+  cout << count;
+  return 0;
 }
