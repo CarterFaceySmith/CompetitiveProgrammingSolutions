5db2672c014403d0279e348b6339b76deb3e2f2c
Carter <carterfs@proton.me>
2024-09-21 20:58:10 +1000

    feat: Add 50A Domino Piling

---
+++ Codeforces/50A_Domino_Piling.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int m, n, sum;
+  cin >> m >> n;
+  cout << div(m * n, 2).quot;
+  return 0;
+}
